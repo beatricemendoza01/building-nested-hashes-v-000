6e34c9956108e5e4583b1a223492cc8412fcd8c4
Beatrice Mendoza <mendoza@nyu.edu>
2018-09-20 02:35:40 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,9 +2,12 @@ def first_challenge
    epic_tragedy = {
      montague: {
        patriarch: {
+         name: "Lord Montague",
+         age: "53"
+       },
+       matriarch: {
          name: 
        },
-       matriarch: {},
        hero: {},
        hero_friends: []
      },
